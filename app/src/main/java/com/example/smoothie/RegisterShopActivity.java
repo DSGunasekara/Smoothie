1b74f11c1543cca394d34881027f91bff9ad9654
ashiMasha <12imashaashini@gmail.com>
2020-10-04 09:59:47 +0530

    shop profile is not working

--- app/src/main/java/com/example/smoothie/RegisterShopActivity.java
+++ app/src/main/java/com/example/smoothie/RegisterShopActivity.java
@@ -1,14 +1,114 @@
 package com.example.smoothie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterShopActivity extends AppCompatActivity {
+
+    EditText shopName, ownerName, shopEmail, shopPwd, shopPhone, shopLocation;
+    Button shopRegBtn;
+    TextView shopLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_shop);
-    }
-}
+
+        shopName = findViewById(R.id.txtShopName);
+        shopEmail = findViewById(R.id.txtShopEmail);
+        ownerName = findViewById(R.id.txtOwnerName);
+        shopPwd = findViewById(R.id.txtShopPwd);
+        shopPhone = findViewById(R.id.txtConNo);
+        shopLocation = findViewById(R.id.txtLocation);
+        shopRegBtn = findViewById(R.id.btnShopReg);
+        shopLoginBtn = findViewById(R.id.btnLoginShopLabel);
+
+        fAuth = FirebaseAuth.getInstance();
+        final FirebaseFirestore fStore;
+        final String shopID;
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            finish();
+        }
+
+        shopLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),ShopLoginActivity.class));
+
+            }
+        });
+
+        shopRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = shopEmail.getText().toString();
+                String password = shopPwd.getText().toString();
+                String phone = shopPhone.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    shopEmail.setError("Email is required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    shopPwd.setError("Enter the password");
+                    return;
+                }
+
+                if (password.length() < 8) {
+                    shopPwd.setError("Password should have minimum 8 characters");
+                    return;
+                }
+
+                if (phone.length() < 10) {
+                    shopPhone.setError("Contact number should have 10 numbers");
+                    return;
+                } else if (phone.length() > 10) {
+                    shopPhone.setError("Contact number cannot have more than 10 numbers");
+                    return;
+                }
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterShopActivity.this, "Account created successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        } else {
+                            Toast.makeText(RegisterShopActivity.this, "Error!!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    //shopID = fAuth.getCurrentUser().getUid(); //retrieve shopID
+                    //DocumentReference documentReference = fStore.collection("Shops").document(shopID);
+                    //Map<String, Object> shop = new HashMap<>();
+                    //user.put("name", name);
+
+                });
+
+
+            }
+
+
+        });
+    }}
---
+++ app/src/main/java/com/example/smoothie/ShopProfileActivity.java
@@ -0,0 +1,152 @@
+package com.example.smoothie;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+public class ShopProfileActivity extends AppCompatActivity {
+
+    TextView appName, shopName, shopLocation, ownerName, shopEmail, shopPhone;
+    ImageView logo;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
+    StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_profile);
+
+        appName = findViewById(R.id.txtAppName2);
+        shopName = findViewById(R.id.txtShopNameLabel);
+        shopLocation = findViewById(R.id.txtShopLocationLabel);
+        ownerName = findViewById(R.id.txtOwnerLabel);
+        shopEmail = findViewById(R.id.txtEmailLabel);
+        shopPhone = findViewById(R.id.txtPhoneLabel);
+        logo = findViewById(R.id.imgLogo);
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        //create directory in firebase storage with user ID
+        StorageReference profileRef = storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(logo);
+
+            }
+        });
+
+        userID = fAuth.getCurrentUser().getUid();
+
+        final DocumentReference documentReference = fStore.collection("Users").document(userID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if(documentSnapshot.exists()) {
+                    ownerName.setText(documentSnapshot.getString("owner"));
+                    shopEmail.setText(documentSnapshot.getString("email"));
+                    shopPhone.setText(documentSnapshot.getString("phone"));
+                    shopName.setText(documentSnapshot.getString("name"));
+                    shopLocation.setText(documentSnapshot.getString("location"));
+                }else{
+                    Log.d("tag","onEvent: document do not exists");
+                }
+
+
+            }
+        });
+
+
+
+       /* btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(view.getContext(),EditUserProfile.class);
+                startActivity(intent);
+
+                intent.putExtra("contact",txtProfPhone.getText().toString());
+                intent.putExtra("name",txtProfName.getText().toString());
+                intent.putExtra("email",txtProfEmail.getText().toString());
+
+                //open Gallery
+//                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                startActivityForResult(openGalleryIntent,1000);
+            }
+        });*/
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1000){
+            if(resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+
+                uploadImageToFirebase(imageUri);
+
+            }
+        }
+
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        //in this we are overwrite same image with new one because user can only have one profile image
+        //upload image to firebase storage
+        final StorageReference fileRef = storageReference.child("users/" + fAuth.getCurrentUser().getUid() + "/profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                //Toast.makeText(UserProfile.this,"Image Uploaded",Toast.LENGTH_SHORT).show();
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(logo);
+
+
+                    }
+                });
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ShopProfileActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+    }
+}
